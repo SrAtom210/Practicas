18b9c737a6158f308ca9afe49af1f08b7a92f6e2
SrAtom210 <mratom210@gmail.com>
2025-09-12 11:53:04 -0600

    se termino el sat(anas)

--- Practicas/app/src/main/java/com/example/practicas/MainActivity.kt
+++ Practicas/app/src/main/java/com/example/practicas/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
@@ -25,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.practicas.ui.theme.PracticasTheme
@@ -87,6 +90,13 @@ fun CalculadoraISR(){
 
     Column (modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center){
+        Row {
+            Image(
+                modifier = Modifier.size(500.dp),
+                painter = painterResource(id = R.drawable.him),
+                contentDescription = null
+            )
+        }
         Row {
             Text(text = "¿Cuanto te va a quitar el SAT(anas) mensualmente?")
         }
